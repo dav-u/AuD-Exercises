c3c9ac8f89ffd07d653348564022c30d8bb54169
David <24206701+dav-u@users.noreply.github.com>
2021-04-12 21:15:26 +0200

    add call to recursive

--- 003_08_PI/main.cpp
+++ 003_08_PI/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 
 double piRecursive(int n) {
-	if (n >= 4) return 1.0;
+	if (n >= 40) return 1.0;
 	return 1 + piRecursive(n + 1) * n / (2*n + 1);
 }
 
@@ -15,6 +15,9 @@ double piIterative(int n) {
 }
 
 void main() {
-	double pi = 2 * piIterative(10);
-	printf("Die ersten Stellen von Pi: %.10lf", pi);
+	double pi = 2 * piRecursive(1);
+	double piIt = 2 * piIterative(40);
+
+	printf("Die ersten Stellen von Pi: %.10lf\n", pi);
+	printf("Iterativ: %.10lf\n", piIt);
 }
